facb9d5de5f3d76f982200fe5cbe35f85f9c2982
Wire-Ghost <evanpanta998@gmail.com>
2024-01-13 15:57:57 +0545

    first commit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function ok() {
    
     let name= document.getElementById("name").value;
     let user= name.toLocaleUpperCase () ;
-    alert("HI"+ ' ' + user + ' ' + "You're a piece of shit! Thank you ."); 
+    alert("HI"+ ' ' + user + ' ' + " "); 
     
 
 
