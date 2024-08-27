aaa094f81af40651e754a6771d29dca2818b6830
Wire-Ghost <evanpanta998@gmail.com>
2024-08-27 16:06:43 +0545

    idk

--- script.js
+++ script.js
@@ -4,4 +4,7 @@ function ok() {
     let name= document.getElementById("name").value;
     let user=name.charAt(0).toUpperCase()+name.slice(1).toLowerCase();
     alert("Hi"+ ' ' + user + ' ' + " "); 
-} 
+
+    window.location.href="idkwhatimmaking.netlify.app";
+
+}
