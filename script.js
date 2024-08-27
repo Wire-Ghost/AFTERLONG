113c64bd6ec99bbf1839fb78160abb48e0743bea
Wire-Ghost <evanpanta998@gmail.com>
2024-08-27 15:47:40 +0545

    idk

--- script.js
+++ script.js
@@ -2,6 +2,6 @@
 function ok() {
    
     let name= document.getElementById("name").value;
-    let user= name.toLocaleUpperCase () ;
-    alert("HI"+ ' ' + user + ' ' + " "); 
+    let user=name.charAt(0).toUpperCase()+name.slice(1).toLowerCase();
+    alert("Hi"+ ' ' + user + ' ' + " "); 
 } 
