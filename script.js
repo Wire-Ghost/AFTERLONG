4800547aaabe54fe63f60b58b5b132adcb3068eb
Wire-Ghost <evanpanta998@gmail.com>
2024-11-17 21:01:02 +0545

    idk

--- script.js
+++ script.js
@@ -22,3 +22,12 @@ function closePopup() {
     document.getElementById('custom-popup').style.display = 'none';
 }
 
+window.addEventListener( "pageshow", function ( event ) {
+    var historyTraversal = event.persisted || 
+                           ( typeof window.performance != "undefined" && 
+                                window.performance.navigation.type === 2 );
+    if ( historyTraversal ) {
+      // Handle page restore.
+      window.location.reload();
+    }
+  });
